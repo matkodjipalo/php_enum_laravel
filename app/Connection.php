240ec5e6e7c9760761195851ff4c3abc7d9f3208
Matko Đipalo <matkodjipalo@gmail.com>
2017-06-16 15:36:45 +0200

    defined accessors & mutators for status

--- app/Connection.php
+++ app/Connection.php
@@ -15,4 +15,24 @@ class Connection extends Model
         'receiver_id',
         'status',
     ];
+
+    /**
+     * @param string $status
+     *
+     * @return string
+     */
+    public function getStatusAttribute($status)
+    {
+        return ConnectionStatusType::createFromString($status);
+    }
+
+    /**
+     * @param ConnectionStatusType $status
+     *
+     * @return string
+     */
+    public function setStatusAttribute(ConnectionStatusType $status)
+    {
+        $this->attributes['status'] = $status->getName();
+    }
 }
